4a020aac45d3695b836ae45eca8d570820658694
eugene <sichi331@gmail.com>
2022-06-07 14:24:29 +0300

    Added color_change screen

---
+++ lib/widgets/color_change_screen.dart
@@ -0,0 +1,46 @@
+import 'dart:math';
+
+import 'package:eugenesoshyn_testtaskflutter/resources/app_colors.dart';
+import 'package:flutter/material.dart';
+
+///Screen with randomized background colors
+class ColorChangeScreen extends StatefulWidget {
+  ///constructor of MyHomePage
+  const ColorChangeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ColorChangeScreen> createState() => _ColorChangeScreenState();
+}
+
+class _ColorChangeScreenState extends State<ColorChangeScreen> {
+  Color _color = const Color(AppColors.startColor);
+
+  void randomizeBackgroundColor() {
+    setState(() {
+      _color = Color(Random().nextInt(AppColors.startColor));
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Test Task'),
+      ),
+      body: GestureDetector(
+        onTap: () => randomizeBackgroundColor(),
+        child: Container(
+          color: _color,
+          child: const Center(
+            child: Text('Hey there'),
+          ),
+        ),
+      ),
+    );
+  }
+}
