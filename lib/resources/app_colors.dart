a05efa5b1c9e3be8b62eed058651fcd180b2d304
eugene <sichi331@gmail.com>
2022-06-08 11:09:43 +0300

    Added function randomizeColor

--- lib/resources/app_colors.dart
+++ lib/resources/app_colors.dart
@@ -1,5 +1,18 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
 ///const colors for application
-abstract class AppColors {
-  ///Color of application at the beginning
-  static const int startColor = 0xffffffff;
+class AppColors {
+  ///Color of application at the beginning.
+  static const int _startColor = 0xffffffff;
+  Color _randomColor = const Color(_startColor);
+
+  ///Get the value of startColor
+  int get startColor => _startColor;
+
+  ///Get the value of  _randomColor
+  Color get randomColor => _randomColor;
+
+  ///Randomize  color of ColorChangeScreen.
+  Color randomizeColor() => _randomColor = Color(Random().nextInt(startColor));
 }
