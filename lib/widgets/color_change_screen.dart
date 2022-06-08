30dd9b3cbabae47afa19a7c1b444f211ba16b34a
eugene <sichi331@gmail.com>
2022-06-08 11:11:39 +0300

    Moved function randomizeColor to app_colors

--- lib/widgets/color_change_screen.dart
+++ lib/widgets/color_change_screen.dart
@@ -1,9 +1,7 @@
-import 'dart:math';
-
 import 'package:eugenesoshyn_testtaskflutter/resources/app_colors.dart';
 import 'package:flutter/material.dart';
 
-///Screen with randomized background colors
+///Screen with randomized background colors.
 class ColorChangeScreen extends StatefulWidget {
   ///constructor of ColorChangeScreen
   const ColorChangeScreen({Key? key}) : super(key: key);
@@ -13,26 +11,23 @@ class ColorChangeScreen extends StatefulWidget {
 }
 
 class _ColorChangeScreenState extends State<ColorChangeScreen> {
-  Color _randomColor = const Color(AppColors.startColor);
-
-  void randomizeBackgroundColor() {
-    setState(() {
-      _randomColor = Color(Random().nextInt(AppColors.startColor));
-    });
-  }
+  final appColors = AppColors();
 
   @override
   void initState() {
     super.initState();
+    appColors.startColor;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GestureDetector(
-        onTap: () => randomizeBackgroundColor(),
+        onTap: () => setState(() {
+          appColors.randomizeColor();
+        }),
         child: Container(
-          color: _randomColor,
+          color: appColors.randomColor,
           child: const Center(
             child: Text(
               'Hey there',
