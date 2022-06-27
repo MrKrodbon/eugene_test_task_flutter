6edc392cc0ebce2f3db69e13df8351cf3860dd7a
eugene <sichi331@gmail.com>
2022-06-27 13:13:11 +0300

    Deleted appColors file

--- lib/resources/app_colors.dart
+++ lib/resources/app_colors.dart
@@ -1,20 +1,2 @@
-import 'dart:math';
-import 'package:flutter/material.dart';
 
-///const colors for application
-class AppColors {
-  ///Color of application at the beginning.
-  static const Color _startColor = Color.fromARGB(0, 255, 0, 0);
 
-  ///Get the value of startColor
-  Color get startColor => _startColor;
-
-  ///Randomize  color of ColorChangeScreen.
-  Color randomizeColor() => Color(
-        Random().nextInt(
-          int.parse(
-            startColor.toString(),
-          ),
-        ),
-      );
-}
